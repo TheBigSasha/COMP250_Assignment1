43f2549f0f57492c0d3ebe74b2006612c04fc6a6
sasha <alexander.aleshchenko@mail.mcgill.ca>
2021-02-05 10:19:45 -0800

    Initial commit

---
+++ src/assignment1/MyAwesomeTester.java
@@ -0,0 +1,10 @@
+package assignment1;
+
+public class MyAwesomeTester {
+	public static void main(String[] args) {
+	    Basket b = new Basket();
+	    b.add(new Egg("Testy test test boiiii", 5,5));
+	    System.out.println(b.getTotalCost());
+	}
+
+}
